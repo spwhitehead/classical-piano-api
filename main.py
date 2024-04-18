1437beee59f72a644787de3b4d362862a061d95f
Spencer Whitehead <53270623+spwhitehead@users.noreply.github.com>
2024-04-18 10:30:21 -0600

    Fully implements database storage for requests

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 
 from contextlib import asynccontextmanager
 from decouple import Config, RepositoryEnv
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
 from models import Composer, Piece
 from sqlmodel import SQLModel, select, Session, create_engine
 
@@ -23,8 +23,10 @@ engine = create_engine(DATABASE_URL)
 async def lifespan(app: FastAPI):
     SQLModel.metadata.create_all(engine)
     await check_and_load_initial_data()
-    yield
-    await shutdown_logic()
+    try:
+        yield
+    finally:
+        await shutdown_logic()
 
 
 async def check_and_load_initial_data():
@@ -51,11 +53,14 @@ async def load_initial_data_from_json():
         session.commit()
 
 
-@asynccontextmanager
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
 async def shutdown_logic():
     engine.dispose()
     logging.info("Database connections closed.")
-    logging.shutdown()
 
 
 app = FastAPI(lifespan=lifespan)
@@ -64,73 +69,101 @@ app = FastAPI(lifespan=lifespan)
 @app.get("/composers")
 async def list_composers():
     with Session(engine) as session:
-        result = session.exec(select(Composer)).all()
+        result = session.exec(
+            select(Composer).order_by(Composer.composer_id)).all()
         return result
 
 
 @app.get("/pieces")
 async def list_pieces():
     with Session(engine) as session:
-        result = session.exec(select(Piece)).all()
+        result = session.exec(select(Piece).order_by(Piece.composer_id)).all()
         return result
-"""
+
+
 @app.post("/composers")
-async def create_composer(person: Composer) -> None:
-    if any(exisiting_composer.name == person.name for exisiting_composer in composers):
+async def create_composer(person: Composer, session: Session = Depends(get_session)) -> str:
+    # Check if a composer with the same name already exists
+    existing_composer = session.exec(
+        select(Composer).where(Composer.composer_id == person.composer_id)).first()
+    if existing_composer:
         raise HTTPException(
-            status_code=400, detail="Composer ID already exists")
-    composers.append(person)
-    return "Composer added successfully"
+            status_code=400, detail="Composer with this name already exists")
+
+    # Add the new composer to the database if they don't already exist
+    session.add(person)
+    session.commit()
+    session.refresh(person)
+    return f"Composer {person.name} added successfully with ID {person.composer_id}"
 
 
 @app.post("/pieces")
-async def create_piece(music: Piece) -> None:
-    if any(existing_piece.name == music.name for existing_piece in pieces):
+async def create_piece(music: Piece, session: Session = Depends(get_session)) -> str:
+    existing_piece = session.exec(
+        select(Piece).where(Piece.name == music.name)).first()
+    if existing_piece:
         raise HTTPException(
-            status_code=400, detail="Piece name already exists")
-    elif any(existing_composer.composer_id == music.composer_id for existing_composer in composers):
-        pieces.append(music)
-    raise HTTPException(
-        status_code=400, detail="No composer with that ID exists.")
+            status_code=400, detail="Piece with this name already exists")
+
+    session.add(music)
+    session.commit()
+    session.refresh(music)
+    return f"Piece {music.name} added successfully. Composer's ID is {music.composer_id}"
 
 
 @app.put("/composers/{composer_id}")
-async def update_composer(composer_id: int, updated_composer: Composer) -> None:
-    if any(existing_composer.composer_id == updated_composer.composer_id for existing_composer in composers):
-        for i, composer in enumerate(composers):
-            if composer.composer_id == composer_id:
-                composers[i] = updated_composer
-                return "Composer updated successfully"
-    return "No composer exists with that ID"
+async def update_composer(composer_id: int, updated_composer: Composer, session: Session = Depends(get_session)) -> str:
+    existing_composer = session.exec(select(Composer).where(
+        Composer.composer_id == composer_id)).first()
+    if existing_composer:
+        session.delete(existing_composer)
+        session.add(updated_composer)
+        session.commit()
+        session.refresh(updated_composer)
+        return f"Composer {updated_composer.name} updated successfully"
+
+    session.add(updated_composer)
+    session.commit()
+    return f"No Composer with ID {composer_id} currently in database. Added {updated_composer.name} with ID {updated_composer.composer_id} successfully"
 
 
 @app.put("/pieces/{piece_name}")
-async def update_piece(piece_name: str, updated_piece: Piece) -> None:
-    if not any(existing_composer.composer_id == updated_piece.composer_id for existing_composer in composers):
-        raise HTTPException(
-            status_code=400, detail="Composer ID does not exist")
-    if any(existing_piece.name == updated_piece.name for existing_piece in pieces):
-        for i, piece in enumerate(pieces):
-            if piece.name == piece_name:
-                pieces[i] = updated_piece
-                return "Piece updated successfully"
-    return "No piece exists with that name"
+async def update_piece(piece_name: str, updated_piece: Piece, session: Session = Depends(get_session)) -> str:
+    existing_piece = session.exec(
+        select(Piece).where(Piece.name == piece_name)).first()
+    if existing_piece:
+        session.delete(existing_piece)
+        session.add(updated_piece)
+        session.commit()
+        session.refresh(updated_piece)
+        return f"Piece {updated_piece.name} updated successfully"
+
+    session.add(updated_piece)
+    session.commit()
+    return f"No Piece with name {piece_name} currently in database. Added {updated_piece.name} successfully"
 
 
 @app.delete("/composers/{composer_id}")
-async def delete_composer(composer_id: int) -> None:
-    for i, person in enumerate(composers):
-        if person.composer_id == composer_id:
-            composers.pop(i)
-            return "Composer deleted successfully"
-    return "No composer exists with that ID"
+async def delete_composer(composer_id: int, session: Session = Depends(get_session)) -> str:
+    existing_composer = session.exec(select(Composer).where(
+        Composer.composer_id == composer_id)).first()
+    if existing_composer:
+        session.delete(existing_composer)
+        session.commit()
+        return f"Composer {existing_composer.name} deleted successfully"
+
+    raise HTTPException(
+        status_code=400, detail=f"No composer existis with ID {composer_id}")
 
 
 @app.delete("/piece/{piece_name}")
-async def delete_piece(piece_name: str) -> None:
-    for i, piece in enumerate(pieces):
-        if piece.name == piece_name:
-            pieces.pop(i)
-            return "Piece deleted successfully"
-    return "No piece exists with that name"
-"""
+async def delete_piece(piece_name: str, session: Session = Depends(get_session)) -> str:
+    existing_piece = session.exec(
+        select(Piece).where(Piece.name == piece_name)).first()
+    if existing_piece:
+        session.delete(existing_piece)
+        session.commit()
+        return f"Piece {existing_piece.name} deleted successfully"
+
+    raise HTTPException(
+        status_code=400, detail=f"No piece exists with name {piece_name}")
